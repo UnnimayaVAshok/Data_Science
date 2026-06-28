894b9f39526bdb790e7ee651e6cf1e9d61fb6944
UnnimayaVAshok <unnimayavashok@gmail.com>
2026-06-28 21:11:36 +0530

    Add Python Basics programs

---
+++ Python/Basics/features.py
@@ -0,0 +1,23 @@
+#input() function
+
+#Type conversion
+
+"""
+number_1 = int(input("Enter the first number"))
+
+number_2 = int(input("Enter the second number"))
+
+sum = number_1 + number_2
+
+print(sum)
+"""
+
+#Dynamically typing
+
+name = "python"
+
+print(type(name))
+
+name = 10
+
+print(type(name))
