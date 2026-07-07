418cf6269ac57421d4c43038b73de236dbe74943
UnnimayaVAshok <unnimayavashok@gmail.com>
2026-07-07 19:38:29 +0530

    pattern works added

--- Python/String/second_char.py
+++ Python/String/second_char.py
@@ -2,14 +2,23 @@
 
 # text = "python" output = "yhn"
 
-text = "python"
+text = "pythontp"
 
 new = ""
 
+"""
 for i in text:
-
+                                    duplicate characters index doest get using this iteration
     if text.index(i) % 2 != 0:
 
         new += i
+
+print(new)
+"""
+for i in range(len(text)):
+
+    if i % 2 != 0:
+
+        new += text[i]
 
 print(new)
---
+++ Python/pattern_works/task_01.py
@@ -0,0 +1,114 @@
+"""
+1
+2
+3
+4
+5
+
+"""
+for i in range(1,6):
+
+    print(i)
+
+"""
+12345
+"""
+for i in range(1,6):
+
+    print(i,end=" ")
+
+"""
+12345
+12345
+12345
+12345
+12345
+"""
+for i in range(1,6):
+
+    for j in range(1,6):
+
+        print(j,end=" ")
+
+    print()
+
+"""
+*****
+*****
+*****
+"""
+
+for i in range(3):
+
+    for j in range(5):
+
+        print("*",end="")
+    print()
+
+"""
+11111
+22222
+33333
+44444
+55555
+"""
+for i in range(1,6):
+
+    for j in range(1,6):
+
+        print(i,end="")
+
+    print()
+
+"""
+55555
+44444
+33333
+22222
+11111
+
+"""
+
+for i in range(5,0,-1):
+
+    for j in range(5):
+
+        print(i,end="")
+
+    print()
+
+print()
+
+"""
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5 
+
+"""
+
+for i in range(1,6):
+
+    for j in range(i):
+        print(j + 1,end = " ")
+
+    print()
+
+print()
+
+"""
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+
+
+1 1 1 1 1
+2 2 2 2
+3 3 3 
+4 4
+5
+
+"""
