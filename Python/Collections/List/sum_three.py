0b3376e0fbd42417cfaeadcabeb48f002313d96d
UnnimayaVAshok <unnimayavashok@gmail.com>
2026-07-16 11:08:47 +0530

    task added

--- Python/Collections/List/sum_three.py
+++ Python/Collections/List/sum_three.py
@@ -3,3 +3,17 @@
 numbers = [1,2,3,4,5,6,7]
 
 target = 9
+
+def sum_three_numbers(elements):
+
+    for i in elements:
+
+        for j in range(i+1, len(elements)+1):
+
+            for k in range(j+1,len(elements)+1):
+
+                if i + j + k == target:
+
+                    print(i,j,k)
+
+sum_three_numbers(numbers)
--- Python/Collections/List/sum_two.py
+++ Python/Collections/List/sum_two.py
@@ -3,3 +3,15 @@
 numbers = [3,4,5,1]
 
 target = 7
+
+def sum_two_numbers(elements):
+
+    for i in elements:
+
+        for j in range(i+1,len(elements) + 1):
+
+            if i + j == target:
+
+                print(i,j)
+
+sum_two_numbers(numbers)
