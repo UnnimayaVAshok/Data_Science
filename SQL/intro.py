0b793704b920f0763b7f93d4f91b668c736419b3
UnnimayaVAshok <unnimayavashok@gmail.com>
2026-08-13 10:49:58 +0530

    python connectivity with sql

--- SQL/intro.py
+++ SQL/intro.py
@@ -1,44 +1,30 @@
 """
-SQL
-=================
-Structured query language
+mysql-connector-python
 
-Data
-=========
-Data is a collection of raw facts and figures
-eg:
-
-id      Name        Place
-
-01      Arun        Kochi
-02      Arun R      Tvm
-03      Rahul       Kochi
-
-purpose
-=========
-Processing the data and organizing it to make it an information
-
-Database
-=============
-Database is a collection of related data
-so it can be easily accessible
-
-Database engines
-================
-Mysql,oracle,postgresq,mongodb
-
-DBMS
-=======
-DBMS is a software allows users to create,retrieve,update and manage the data in the database
-
-user----communicate(SQL)------DBMS-----------Database
-=========================================================
-
-==============
-create 
-insert
-update
-delete
-
-create database <databasename>;
 """
+import mysql.connector
+
+connector = mysql.connector.connect(
+    host="localhost",            # server ip or host name
+    user="root",                 # your mysql username
+    password="root",             # your mysql password
+    database="company_new"       # the specific database to use
+)
+
+if connector.is_connected():
+    print("Database connected")
+
+cursor = connector.cursor()
+"""
+cursor.execute("select * from employee")
+rows = cursor.fetchall()
+for i in rows:
+    print(i)
+"""
+cursor.execute("select location,count(*) from employee group by location;")
+rows = cursor.fetchall()
+for i in rows:
+    print(i)
+
+cursor.close()
+connector.close()
---
+++ SQL/introduction.py
@@ -0,0 +1,44 @@
+"""
+SQL
+=================
+Structured query language
+
+Data
+=========
+Data is a collection of raw facts and figures
+eg:
+
+id      Name        Place
+
+01      Arun        Kochi
+02      Arun R      Tvm
+03      Rahul       Kochi
+
+purpose
+=========
+Processing the data and organizing it to make it an information
+
+Database
+=============
+Database is a collection of related data
+so it can be easily accessible
+
+Database engines
+================
+Mysql,oracle,postgresq,mongodb
+
+DBMS
+=======
+DBMS is a software allows users to create,retrieve,update and manage the data in the database
+
+user----communicate(SQL)------DBMS-----------Database
+=========================================================
+
+==============
+create 
+insert
+update
+delete
+
+create database <databasename>;
+"""
